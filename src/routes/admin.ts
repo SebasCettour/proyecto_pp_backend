0bdb1f7968569d2003e6748cb63ca5f6843eba8d
Sebastián Cettour <sebas.cettour83@gmail.com>
2025-09-09 00:32:07 -0300

    Correcciones

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { authenticateToken } from "../middleware/authMiddleware";
+import { authenticateToken } from "middleware/authMiddleware.js";
 
 const router = Router();
 
